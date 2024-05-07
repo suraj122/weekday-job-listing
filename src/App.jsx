fd2e65e20816388455e77e5552fefb5ba91ea877
Mukesh kumar <122suraj0@gmail.com>
2024-05-07 16:40:33 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h3" component="h1" align="center">
+        Hello Weekday!
+      </Typography>
+    </>
+  );
+}
+
+export default App;
